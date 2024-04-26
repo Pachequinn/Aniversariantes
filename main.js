1bddc7982d5603b39105491b27616578a39fe5a6
pachequinho <104856730+Pachequinn@users.noreply.github.com>
2024-04-26 10:04:10 -0300

    .

--- main.js
+++ main.js
@@ -25,9 +25,9 @@ const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 
 function atualizaCronometro(){ 
-    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];  
-    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+    document.getElementById("dias0").textContent = calculaTempo(tempos[364])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[14])[1];  
+    document.getElementById("min0").textContent = calculaTempo(tempos[56])[2];
     document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
     for (let i=0; i<contadores.length;i++){
         //contadores[i].textContent = calculaTempo(tempos[i]);   
